664e64d3d22c4d885fdffa8f383741ff54d23fbe
jpratt-mru <jpratt@mtroyal.ca>
2017-10-01 14:58:39 -0600

    add sample 03

---
+++ sample-03.java
@@ -0,0 +1,13 @@
+final int SQUID_POINTS = 3; 
+final int SALMON_POINTS = 2;
+final int EGG_POINTS = 1;
+final int TEMPURA_POINTS = 5;
+final int TEMPURA_POINTS2 = 2;
+final int SASHIMI_POINTS = 10;
+final int SASHIMI_POINTS2 = 3;
+final int DUMPLING_POINTS = 1;
+final int DUMPLING_POINTS2 = 2;
+final int DUMPLING_MAX_POINTS = 15;
+
+System.out.print("Please enter name here: ");
+String name = in.next(); 
